ae377a5e47980bda9c2d801220abfad2574f2ded
Loic BASTIAN <lbastian@paul-f4Br4s7.clusters.42paris.fr>
2024-01-15 14:27:13 +0100

    Add get_time_t

--- includes/Sessions.hpp
+++ includes/Sessions.hpp
@@ -6,7 +6,7 @@
 /*   By: lbastian <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 17:08:35 by lbastian          #+#    #+#             */
-/*   Updated: 2024/01/11 18:47:59 by lbastian         ###   ########.fr       */
+/*   Updated: 2024/01/15 14:13:26 by lbastian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ class Sessions
 	private:
 		Sessions(const Sessions& sessions);
 		Sessions& operator=(const Sessions& sessions);
-		std::map<std::string, std::map<int, time_t> > _sessions_data;
+		std::map<std::string, std::map<int, time_t> >	_sessions_data;
+		int 											check_date(int day, int month, int year);
 
 	public:
 		Sessions(void);
@@ -31,6 +32,7 @@ class Sessions
 		int		create_token(void);
 		void	add(std::string username, int token, time_t date);
 		bool	verify(std::string username, int token);
+		time_t	get_time_t(int sec, int min, int hour, int day, int month, int year);
 };
 
 
--- srcs/Sessions.cpp
+++ srcs/Sessions.cpp
@@ -6,7 +6,7 @@
 /*   By: lbastian <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 17:07:16 by lbastian          #+#    #+#             */
-/*   Updated: 2024/01/11 18:48:10 by lbastian         ###   ########.fr       */
+/*   Updated: 2024/01/15 14:13:28 by lbastian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,34 @@ bool Sessions::verify(std::string username, int token)
 	if (it_inside->second < actual)
 		return (false);
 	return (true);
+}
+
+int Sessions::check_date(int day, int month, int year)
+{
+	int days_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		if (month == 2 && day == 29)
+			return (0);
+	if (day == 0 || day > days_month[month - 1])
+		return (1);
+	return (0);
+
+}
+
+time_t Sessions::get_time_t(int sec, int min, int hour, int day, int month, int year)
+{
+	time_t result;
+	tm tm = {};
+
+	if (sec > 59 || sec < 0 || min > 59 || min < 0 || hour < 0 || hour > 23 || day < 1 || day > 31 || month < 1 || month > 12 || year < 1900 || check_date(day, month, year))
+		return (-1);
+	tm.tm_sec = sec;
+	tm.tm_min = min;
+	tm.tm_hour = hour;
+	tm.tm_mday = day;
+	tm.tm_mon = month - 1;
+	tm.tm_year = year - 1900;
+	result = mktime(&tm);
+	return (result);
 }
