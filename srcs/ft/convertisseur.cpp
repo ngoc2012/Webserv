4022886db921dd0f5cd03dd48a7f7aed228e8b83
Nathan <nathan.bechon@free.fr>
2024-03-04 08:45:25 +0100

    convertisseur

---
+++ srcs/ft/convertisseur.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convertisseur.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbechon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 08:42:44 by nbechon           #+#    #+#             */
+/*   Updated: 2023/12/23 15:22:23 by nbechon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+std::string ft(size_t size) {
+    std::ostringstream result;
+    if (size < 1024) {
+        result << size << "b";
+    } else if (size < 1024 * 1024) {
+        result << size / 1024 << "Kb";
+    } else {
+        result << size / (1024 * 1024) << "Mb";
+    }
+    return result.str();
+}
