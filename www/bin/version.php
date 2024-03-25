ab9e68dda89997d39f0375be1f2b7568a89c2263
Loic BASTIAN <lbastian@paul-f4Ar2s4.clusters.42paris.fr>
2024-03-25 10:50:39 +0100

    Correct version.php

--- www/bin/version.php
+++ www/bin/version.php
@@ -1,7 +1,8 @@
 #!/usr/bin/php-cgi
 <?php
 // Content-type header to specify that it's a PHP script
-header("Content-type: text/plain");
+echo "Status: 200 OK\r\n";
+echo "Content-Type: plain/text; charset=utf-8\r\n\r\n";
 
 echo "Content-type: text/plain\n\n";
 echo "PHP CGI Test Script\n\n";
