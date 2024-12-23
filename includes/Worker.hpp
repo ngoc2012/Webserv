a08baf263764f0f5a3b59134e266a6a65c42958f
ngoc2012 <ngoc2012@yahoo.com>
2024-12-23 08:32:01 +0100

    tempory

--- includes/Worker.hpp
+++ includes/Worker.hpp
@@ -33,24 +33,24 @@ class	Worker
         int         _id;
         pthread_t   _th;
         
-        Host*       _host;
-        int			_sk_ready;
-        int			_max_sk;		    // Max of all fd
+        Host* _host;
+        int   _sk_ready;
+        int   _max_sk;		    // Max of all fd
         
-        volatile bool		_terminate_flag;
+        volatile bool _terminate_flag;
         
-        std::map<int, Request*>		_sk_request;
-        std::map<int, time_t>		_sk_timeout;
-        std::map<int, Address*>		_sk_address;
+        std::map<int, Request*> _sk_request;
+        std::map<int, time_t>   _sk_timeout;
+        std::map<int, Address*> _sk_address;
         
-        pthread_mutex_t		_set_mutex;
-        pthread_mutex_t		_terminate_mutex;
+        pthread_mutex_t _set_mutex;
+        pthread_mutex_t _terminate_mutex;
         // Host should know if fd_out exist
         // Cgi should write to fd_out
-        pthread_mutex_t		_fd_out_mutex;
-        pthread_mutex_t		_timeout_mutex;
-        pthread_cond_t		_cond_set_updated;
-        bool				_set_updated;
+        pthread_mutex_t _fd_out_mutex;
+        pthread_mutex_t _timeout_mutex;
+        pthread_cond_t  _cond_set_updated;
+        bool            _set_updated;
         
         Worker(const Worker&);
         Worker	&operator=(const Worker& op);
@@ -73,9 +73,9 @@ class	Worker
         Host*       		get_host(void) const;
         pthread_mutex_t*	get_set_mutex(void);
         pthread_cond_t*		get_cond_set_updated(void);
-        bool				get_terminate_flag(void);
-        bool				get_set_updated(void);
-        std::map<int, Request*>*		get_sk_request(void);
+        bool			get_terminate_flag(void);
+        bool			get_set_updated(void);
+        std::map<int, Request*>* get_sk_request(void);
         
         void        set_id(int);
         void        set_host(Host*);
