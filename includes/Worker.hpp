babc77589f0a21d7ac613c34e87a01c0690febb8
ngoc2012 <ngoc2012@yahoo.com>
2024-12-23 08:30:02 +0100

    tempory

--- includes/Worker.hpp
+++ includes/Worker.hpp
@@ -28,58 +28,58 @@ class	Configuration;
 
 class	Worker
 {
-	private:
+    private:
+    
+        int         _id;
+        pthread_t   _th;
         
-		int         _id;
-		pthread_t   _th;
-	
-		Host*       _host;
-		int			_sk_ready;
-		int			_max_sk;		    // Max of all fd
-		
-		volatile bool		_terminate_flag;
-
-		std::map<int, Request*>		_sk_request;
-		std::map<int, time_t>		_sk_timeout;
-		std::map<int, Address*>		_sk_address;
-
-		pthread_mutex_t		_set_mutex;
-		pthread_mutex_t		_terminate_mutex;
-		// Host should know if fd_out exist
-		// Cgi should write to fd_out
-		pthread_mutex_t		_fd_out_mutex;
-		pthread_mutex_t		_timeout_mutex;
-		pthread_cond_t		_cond_set_updated;
-		bool				_set_updated;
-
-		Worker(const Worker&);
-		Worker	&operator=(const Worker& op);
-
-	public:
-		Worker();
-		virtual ~Worker();
-
-		void    	routine(void);
-
-		void		new_connection(int, Address*);
-		void		close_client_sk(int);
-		void		set_sk_timeout(int);
-		void		set_end(void);
-		void		check_timeout(void);
-		void		wait_for_set_updated(void);
-
+        Host*       _host;
+        int			_sk_ready;
+        int			_max_sk;		    // Max of all fd
+        
+        volatile bool		_terminate_flag;
+        
+        std::map<int, Request*>		_sk_request;
+        std::map<int, time_t>		_sk_timeout;
+        std::map<int, Address*>		_sk_address;
+        
+        pthread_mutex_t		_set_mutex;
+        pthread_mutex_t		_terminate_mutex;
+        // Host should know if fd_out exist
+        // Cgi should write to fd_out
+        pthread_mutex_t		_fd_out_mutex;
+        pthread_mutex_t		_timeout_mutex;
+        pthread_cond_t		_cond_set_updated;
+        bool				_set_updated;
+        
+        Worker(const Worker&);
+        Worker	&operator=(const Worker& op);
+    
+    public:
+        Worker();
+        virtual ~Worker();
+        
+        void    	routine(void);
+        
+        void		new_connection(int, Address*);
+        void		close_client_sk(int);
+        void		set_sk_timeout(int);
+        void		set_end(void);
+        void		check_timeout(void);
+        void		wait_for_set_updated(void);
+    
         pthread_t*  		get_th(void);
         int         		get_id(void) const;
         Host*       		get_host(void) const;
         pthread_mutex_t*	get_set_mutex(void);
         pthread_cond_t*		get_cond_set_updated(void);
-		bool				get_terminate_flag(void);
+        bool				get_terminate_flag(void);
         bool				get_set_updated(void);
         std::map<int, Request*>*		get_sk_request(void);
-
+        
         void        set_id(int);
         void        set_host(Host*);
-		void		set_terminate_flag(bool);
+        void		set_terminate_flag(bool);
         void		set_set_updated(bool);
 };
 
